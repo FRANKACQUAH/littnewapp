1c7afc5c9daddc29ad04307e5f776ffe7bb4837c
Frank Acquah <97830149+FRANKACQUAH@users.noreply.github.com>
2022-02-08 02:26:38 -0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,472 @@
+<template>
+<!-- An element should be left before coding starts. Ex: Class or Id-->
+<!-- All codes should be within the first element (<div) as well as the template-->
+<div id="app"> 
+<HelloWorld/>
+<!-- My hero section -->
+<section id="header">
+<div class="navbar" >
+
+</div>
+</section>
+  <section id="hero">
+  <div class="hero container">
+  <div>
+  <!-- Text reveal effect. Every text effect should have a diff tag -->
+   <h1>My, <span></span></h1>
+   <h1>Lean Icon Technology <span></span></h1>
+   <h1>News App <span></span></h1>
+   <!-- This is my button -->
+   <a href="#" type="button" class="cta">Get Started</a>
+   </div>
+  </div>
+  </section>
+<!-- End of section -->
+<!-- Services Section-->
+<section id="services">
+<div class="services container">
+<div class="service-top">
+<h1 class="section-title">Serv<span>i</span>ces</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed!</p>
+</div>
+<!-- Card for containing content of services-->
+<div class="service-bottom">
+<div class="service-item">
+<div class="icon"><img src="./assets/img/news.png"></div>
+<h2>Publish News/Article</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore,<a href="https://google.com" style="color:crimson">Read more</a></p>
+</div>
+<div class="service-item">
+<div class="icon"><img src="./assets/img/event.png"></div>
+<h2>Publish Events</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, <a href="https://google.com" style="color:crimson">Read more</a></p>
+</div>
+<div class="service-item">
+<div class="icon"><img src="./assets/img/video.png"></div>
+<h2>Publish Videos</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, <a href="https://google.com" style="color:crimson">Read more</a></p>
+</div>
+<div class="service-item">
+<div class="icon"><img src="./assets/img/consulting.png"></div>
+<h2>Consulting & Training</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, 
+placeat quod enim eum architecto sed! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero, ut perspiciatis officia odio 
+nostrum animi sint enim maiores dicta? Temporibus tempore at deserunt, <a href="https://google.com" style="color:crimson">Read more</a></p>
+</div>
+</div>
+</div>
+</section>
+<!-- End of Services Section-->
+<!-- Begening published project section (This where all works will be filtered for publishing)-->
+<section class="section-title">
+<h1>Published <span>Projects</span></h1>
+</section>
+<section class="container">
+<div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+</div>
+</section>
+<section class="container">
+<div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="./assets/img/man.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Project Title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn" style="background-color:crimson">Read more</a>
+  </div>
+</div>
+</section>
+ <!-- End of card -->
+  <!-- Begining of contact us -->
+<footer id="footer" style="background-color:rgb(26, 24, 24)">
+<p class="footer-text">Copyright @2022 My Leanicon Technology App. All rights reserved.</p>
+</footer>
+    <!--End of Footer -->
+</div>
+  <!-- End of element -->
+</template>
+
+<script>
+/*This founction should not be cleared.It can only be modified to call another element*/
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+    /*All other components can be called here within the javascript and referenced at the begining of App.vue*/
+  }
+}
+</script>
+
+<style>
+ /*Auto body styling*/
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+ /*Button anchor decoration*/
+a{
+  text-decoration:none;
+}
+ /*Hero container styling*/
+.container{
+  min-height:100vh;
+  width:100%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+ /*Hero Styling*/
+}
+#hero{
+  background-size:cover;
+  background-position:top center;
+  position: relative;
+  /*Will change background image for a better display*/
+  background-image:url(./assets/img/nana.jpg)
+  /*#hero::after (color-overlay) Not Working*/
+  }
+#hero::after {       
+background-color:black;
+position:absolute;
+left:0;
+top:0;
+height:100%;
+width:100%;
+}
+#hero h1{
+display:block;
+width:fit-content;
+font-size:4rem;
+position:relative;
+/*Text transition animation update*/
+color:transparent;
+animation:text_reveal .5s ease forwards;
+animation-delay: .3s;
+}
+/*Timing functionality on text*/
+#hero h1:nth-child(1){
+animation-delay:1s;
+}
+#hero h1:nth-child(2){
+animation-delay:2s;
+}
+#hero h1:nth-child(3){
+animation-delay:2s;
+animation:text_reveal_name .5s ease forwards;
+}
+/*Span delay effect*/
+#hero h1 span:nth-child(1){
+animation-delay:.5s;
+}
+#hero h1 span:nth-child(2){
+animation-delay:1.5s;
+}
+#hero h1 span:nth-child(3){
+animation-delay:2s;
+}
+/*Alignning text display*/
+#hero .hero{
+max-width: 1200PX;
+margin:0 auto;
+padding:0 50px;
+justify-content:flex-start;
+}
+ /*Using the button class of "cta" to style  button*/
+#hero .cta {
+display:inline-block;
+color:wheat;
+padding:20px 30px;
+background-color: transparent;
+border:2px solid crimson;
+text-transform: uppercase;
+letter-spacing:1rem;
+margin-top:30px;
+transition:.3s ease;
+transition-property:background, color;
+}
+#hero .cta:hover{
+color:white;
+background-color: crimson;
+/*Transition styling*/
+}
+#hero h1 span{
+position:absolute;
+top:0;
+left:0;
+height:100%;
+width:0;
+background-color: crimson;
+animation:text_reveal_box 1s ease;
+animation-delay:1s;
+}
+/*Keyframes for text reveal box size*/
+@keyframes text_reveal_box{
+ 50%{
+ width:100%;
+ left:0;
+ }
+ 100%{
+ width:0;
+ left:100%;
+ }
+}
+/*For controlling text colour*/
+@keyframes text_reveal{
+ 100%{
+ color:white;
+ }
+}
+/*Changed last text color and weight*/
+@keyframes text_reveal_name{
+ 100%{
+ color:crimson;
+ font-weight:500px;
+ }
+} /*End of hero styling*/
+/*Services styling*/
+#services .services{
+flex-direction:column;
+max-width:1500px;
+margin:auto;
+text-align:center;
+padding:100px 0;
+}
+.section-title{
+  font-size:3rem;
+  text-align:center;
+  font-weight:300;
+  color:black;
+  margin-bottom:10px;
+  text-transform:uppercase;
+  letter-spacing:2rem;
+}
+#services .section-title p{
+  color:white;
+  font-size:1.4rem;
+  margin-top:5px;
+  letter-spacing:.05rem;
+  line-height:2.5rem;
+}
+#services .section-bottom{
+  display:flex;
+  text-align:center;
+  justify-content: center;
+  flex-wrap:wrap;
+} 
+#services .service-item{
+  flex-basis: 70%;
+  display:flex;
+  flex-direction:column;
+  align-items:flex-start;
+  justify-content:center;
+  padding:30px;
+  border-radius:10px;
+  background-image:url(./assets/img/code.jpg);
+  background-size:cover;
+  margin:10px 5%;
+  position:relative;
+   z-index:1;
+   overflow:hidden;
+}
+#services .service-item::after{
+  content:'';
+  position:absolute;
+  left:0;
+  top: 0;
+  height:100%;
+  width:100%;
+  background-color: black;
+  opacity:.4;
+  z-index:-1;
+}
+#services .service-item h2{
+  font-size:2rem;
+  text-transform:uppercase;
+  font-weight:bold;
+  color:white;
+}
+#services .service-item p{
+  font-size:18px;
+  color:white;
+  text-align:left;
+  line-height:1.9rem;
+}
+p{
+  font-size:18px;
+  color:black;
+  margin:10px;
+  text-align:center;
+  line-height:1.9rem;
+}
+#services .service-bottom .icon{
+  height:80px;
+  width:80px;
+  margin-bottom:20px;
+}
+img{
+height:100%;
+width:100%;
+object-fit: cover;
+}
+/*styling the project section*/
+#projects .project{
+  flex-direction:column;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 100px 0;
+
+}
+/*End of styling the project section*/
+/*Styling for published projects*/
+.card{
+background-color:rgb(26, 24, 24);
+color:white;
+margin-top:3px;
+margin:5px;
+padding:5px;
+text-align:center;
+text-transform:uppercase;
+border-radius:2rem;
+}
+.card-img-top{
+border-radius:2rem;
+}
+.card-text{
+color:white;
+text-transform:lowercase;
+font-size:14px;
+}
+.card-title{
+ font-size:15px;
+}
+/*Copntact section styling*/
+#contact .contact{
+flex-direction: column;
+max-width:1200px;
+margin:0 auto;
+}
+#contact .contact-items{
+width: 500px;
+}
+#contact .contact-item{
+align-items:center;
+text-align:center;
+width: 80%;
+border-radius:10px;
+margin:30px;
+padding:30px;
+justify-content:center;
+display:flex;
+box-shadow: 0px 0px 18px 0px #0000002c;
+flex-direction:column;
+transition: .3s ease box-shadow;
+}
+#contact .contact-item:hover{
+box-shadow: 0px 0px 15px 0px #0000002c;
+}
+#contact .icon{
+width:70px;
+height:70px;
+margin:0 auto;
+margin-bottom:10px;
+margin-top:10px;
+}
+#contact .contact-info h1{
+ font-size:2.5rem;
+ font-weight:500;
+ margin-bottom:5px;
+}
+#contact .contact-info h2{
+ font-size:1.5rem;
+ font-weight:500;
+ margin-bottom:5px;
+ line-height:2rem;
+font-weight:500;
+}
+#footer{
+ height:0 auto;
+}
+
+.footer-text{
+ color:white;
+}
+/*End of contact section styling*/
+/*Footer styling*/
+/*End of styling*/
+#app {
+/*General app style. This can be deleted since we have already styled the body of the project*/
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top:0;
+}
+</style>
